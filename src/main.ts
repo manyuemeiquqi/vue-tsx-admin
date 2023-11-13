7953f05b2b74f615821db2881390c102b15755df
manyuemeiquqi <940495614@qq.com>
2023-11-13 18:58:06 +0800

    feat: add tailwind and arco component library

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import './assets/main.css'
+import './assets/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import ArcoVue from '@arco-design/web-vue'
+import '@arco-design/web-vue/dist/arco.css'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.use(ArcoVue)
 
 app.use(createPinia())
 app.use(router)
