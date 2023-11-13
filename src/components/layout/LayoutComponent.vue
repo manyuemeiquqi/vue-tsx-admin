d79ae5411a882d0601947e957b21dbb081a5b4d0
manyuemeiquqi <940495614@qq.com>
2023-11-13 23:58:19 +0800

    feat: add layout component

---
+++ src/components/layout/LayoutComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <ALayout class="">
+    <header>header</header>
+    <ALayout>
+      <ALayoutSider> menu </ALayoutSider>
+      <ALayout>
+        <div>tab close</div>
+        <ALayoutContent class="min-h-screen">
+          <PageComponent />
+        </ALayoutContent>
+      </ALayout>
+    </ALayout>
+  </ALayout>
+</template>
+
+<script lang="ts" setup>
+import PageComponent from '@/components/layout/page-component/PageComponent.vue'
+</script>
+
+<style lang="scss" scoped></style>
