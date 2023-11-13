ba6b7bd2e70bc0cad58dfc7358dd215ee0a1ca22
manyuemeiquqi <940495614@qq.com>
2023-11-13 18:58:35 +0800

    feat: add login view

---
+++ src/views/MainView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col">
+    <header class="fixed">1212</header>
+    <div class="flex-1">
+      <div class="flex flex-row">
+        <div>menu</div>
+        <!-- <a-menu></a-menu> -->
+        <div class="min-h-screen">main</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
