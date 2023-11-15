f8260920915eceda4321029be8267d931f22fef3
manyuemeiquqi <940495614@qq.com>
2023-11-15 11:36:36 +0800

    chore: update add import

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { createPinia } from 'pinia'
 
 import ArcoVue from '@arco-design/web-vue'
 import '@arco-design/web-vue/dist/arco.css'
-import App from './App.vue'
+import App from './App'
 import router from './router'
 
 const app = createApp(App)
