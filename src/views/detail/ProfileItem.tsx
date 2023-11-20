588644fbf44d1ba97f1f121316ac28f190ecc9fc
manyuemeiquqi <940495614@qq.com>
2023-11-20 14:32:44 +0800

    feat: detail view

--- src/views/detail/ProfileItem.tsx
+++ src/views/detail/ProfileItem.tsx
@@ -1,7 +1,144 @@
-import { defineComponent } from 'vue'
-
+import { Card, Descriptions, Skeleton } from '@arco-design/web-vue'
+import { defineComponent, type PropType } from 'vue'
+import { useI18n } from 'vue-i18n'
+interface ProfileBasicRes {
+  status: number
+  video: {
+    mode: string
+    acquisition: {
+      resolution: string
+      frameRate: number
+    }
+    encoding: {
+      resolution: string
+      rate: {
+        min: number
+        max: number
+        default: number
+      }
+      frameRate: number
+      profile: string
+    }
+  }
+  audio: {
+    mode: string
+    acquisition: {
+      channels: number
+    }
+    encoding: {
+      channels: number
+      rate: number
+      profile: string
+    }
+  }
+}
 export default defineComponent({
-  setup() {
-    return () => <div></div>
+  props: {
+    profileData: {
+      type: Object as PropType<any>
+    },
+    loading: {
+      type: Boolean
+    }
+  },
+  setup(props) {
+    console.log('props: ', props)
+
+    const { t } = useI18n()
+    const data = props.profileData
+    const blockDataList: {
+      title: string
+      data: {
+        label: string
+        value: string
+      }[]
+    }[] = []
+    blockDataList.push({
+      title: t(`basicProfile.title.${'current'}Video`),
+      data: [
+        {
+          label: t('basicProfile.label.video.mode'),
+          value: data?.video?.mode || '-'
+        },
+        {
+          label: t('basicProfile.label.video.acquisition.resolution'),
+          value: data?.video?.acquisition.resolution || '-'
+        },
+        {
+          label: t('basicProfile.label.video.acquisition.frameRate'),
+          value: `${data?.video?.acquisition.frameRate || '-'} fps`
+        },
+        {
+          label: t('basicProfile.label.video.encoding.resolution'),
+          value: data?.video?.encoding.resolution || '-'
+        },
+        {
+          label: t('basicProfile.label.video.encoding.rate.min'),
+          value: `${data?.video?.encoding.rate.min || '-'} bps`
+        },
+        {
+          label: t('basicProfile.label.video.encoding.rate.max'),
+          value: `${data?.video?.encoding.rate.max || '-'} bps`
+        },
+        {
+          label: t('basicProfile.label.video.encoding.rate.default'),
+          value: `${data?.video?.encoding.rate.default || '-'} bps`
+        },
+        {
+          label: t('basicProfile.label.video.encoding.frameRate'),
+          value: `${data?.video?.encoding.frameRate || '-'} fpx`
+        },
+        {
+          label: t('basicProfile.label.video.encoding.profile'),
+          value: data?.video?.encoding.profile || '-'
+        }
+      ]
+    })
+
+    blockDataList.push({
+      title: t(`basicProfile.title.${'origin'}Audio`),
+      data: [
+        {
+          label: t('basicProfile.label.audio.mode'),
+          value: data?.audio?.mode || '-'
+        },
+        {
+          label: t('basicProfile.label.audio.acquisition.channels'),
+          value: `${data?.audio?.acquisition.channels || '-'} ${t(
+            'basicProfile.unit.audio.channels'
+          )}`
+        },
+        {
+          label: t('basicProfile.label.audio.encoding.channels'),
+          value: `${data?.audio?.encoding.channels || '-'} ${t('basicProfile.unit.audio.channels')}`
+        },
+        {
+          label: t('basicProfile.label.audio.encoding.rate'),
+          value: `${data?.audio?.encoding.rate || '-'} kbps`
+        },
+        {
+          label: t('basicProfile.label.audio.encoding.profile'),
+          value: data?.audio?.encoding.profile || '-'
+        }
+      ]
+    })
+    return () => (
+      <Card>
+        {blockDataList.map((item) => (
+          <Descriptions title={item.title} data={item.data}>
+            {{
+              value: () =>
+                props.loading ? (
+                  <Skeleton>
+                    <Skeleton.Line rows={1}></Skeleton.Line>
+                  </Skeleton>
+                ) : (
+                  <span></span>
+                )
+            }}
+          </Descriptions>
+        ))}
+      </Card>
+    )
   }
 })
--- src/views/detail/index.tsx
+++ src/views/detail/index.tsx
@@ -2,15 +2,63 @@ import { defineComponent, ref } from 'vue'
 import DataUpdateRecord from '@/views/detail/DataUpdateRecord'
 import ProfileItem from '@/views/detail/ProfileItem'
 import { Card, Space, Steps, Button } from '@arco-design/web-vue'
+import axios from 'axios'
+import useLoading from '@/hooks/loading'
+
+interface ProfileBasicRes {
+  status: number
+  video: {
+    mode: string
+    acquisition: {
+      resolution: string
+      frameRate: number
+    }
+    encoding: {
+      resolution: string
+      rate: {
+        min: number
+        max: number
+        default: number
+      }
+      frameRate: number
+      profile: string
+    }
+  }
+  audio: {
+    mode: string
+    acquisition: {
+      channels: number
+    }
+    encoding: {
+      channels: number
+      rate: number
+      profile: string
+    }
+  }
+}
 export default defineComponent({
   setup() {
     const currentStatus = ref(1)
+    const currentData = ref({})
+    const preData = ref({})
     const stepList = [
       { status: 1, label: '提交更改' },
       { status: 2, label: '审批中' },
       { status: 3, label: '修改完成' }
     ]
-
+    const { loading, setLoading } = useLoading(true)
+    const fetchData = async () => {
+      try {
+        const data = (await axios.get('/api/profile/basic')).data as ProfileBasicRes
+        currentData.value = data
+        preData.value = data
+      } catch (error) {
+        console.log('error: ', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchData()
     return () => (
       <div class=" h-screen">
         <Space size={16} direction="vertical">
@@ -32,10 +80,10 @@ export default defineComponent({
             }}
           </Card>
           <Card>
-            <ProfileItem></ProfileItem>
+            <ProfileItem loading={loading.value} profileData={currentData.value}></ProfileItem>
           </Card>
           <Card>
-            <ProfileItem></ProfileItem>
+            <ProfileItem loading={loading.value} profileData={preData.value}></ProfileItem>
           </Card>
           <DataUpdateRecord></DataUpdateRecord>
         </Space>
