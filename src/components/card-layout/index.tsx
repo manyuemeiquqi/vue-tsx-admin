66d8ffa1f728b06003e7fa18281fe7174e1bac27
manyuemeiquqi <940495614@qq.com>
2023-12-03 11:56:52 +0800

    feat: develop card layout

---
+++ src/components/card-layout/index.tsx
@@ -0,0 +1,10 @@
+import { defineComponent } from 'vue'
+interface Slots {
+  default: any
+}
+export default defineComponent({
+  name: 'CardLayout',
+  setup(_, { slots }) {
+    return () => <div class="content-wrapper">{(slots as unknown as Slots).default()}</div>
+  }
+})
