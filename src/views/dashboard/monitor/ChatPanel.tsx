8e038a383550fca262a811b7499ec68bea2df7f5
manyuemeiquqi <940495614@qq.com>
2023-12-04 22:53:26 +0800

    feat: add monitor style

--- src/views/dashboard/monitor/ChatPanel.tsx
+++ src/views/dashboard/monitor/ChatPanel.tsx
@@ -1,8 +1,9 @@
-import { Button, Card, Grid, Input, Link, Select, Space } from '@arco-design/web-vue'
+import { Button, Card, Grid, Input, Link, Option, Select, Space } from '@arco-design/web-vue'
 import { IconDownload, IconFaceSmileFill } from '@arco-design/web-vue/es/icon'
 import axios from 'axios'
 import { defineComponent, ref } from 'vue'
 import MessageItem from './MessageItem'
+import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
   setup() {
@@ -10,8 +11,7 @@ export default defineComponent({
     const fetchData = async () => {
       try {
         const { data } = await axios.post<any>('/api/message/list')
-        console.log('data: ', data)
-        chatData.value = data.data
+        chatData.value = data
       } catch (err) {
         // you can report use errorHandler or other
       } finally {
@@ -19,11 +19,14 @@ export default defineComponent({
       }
     }
     fetchData()
+    const { t } = useI18n()
     return () => (
-      <Card>
-        <Space>
-          <Select></Select>
-          <Input></Input>
+      <Card class="general-card" title={t('monitor.title.chatPanel')}>
+        <Space size="small">
+          <Select defaultValue="all">
+            <Option value="all">{t('monitor.chat.options.all')}</Option>
+          </Select>
+          <Input placeholder={t('monitor.chat.placeholder.searchCategory')}></Input>
           <Link>
             <IconDownload />
           </Link>
@@ -31,13 +34,13 @@ export default defineComponent({
         {chatData.value.map((item) => (
           <MessageItem data={item} />
         ))}
-        <Space>
+        <Space size="small">
           <Input>
             {{
               suffix: () => <IconFaceSmileFill />
             }}
           </Input>
-          <Button>112</Button>
+          <Button type="primary">{t('monitor.chat.update')}</Button>
         </Space>
       </Card>
     )
--- src/views/dashboard/monitor/LivePanel.tsx
+++ src/views/dashboard/monitor/LivePanel.tsx
@@ -1,24 +1,102 @@
-import { RadioGroup, Table, Space, Tabs, Card, Avatar, Typography } from '@arco-design/web-vue'
+import { useUserStore } from '@/store'
+import {
+  RadioGroup,
+  Table,
+  Space,
+  Tabs,
+  Card,
+  Avatar,
+  Typography,
+  Radio,
+  Button,
+  Link
+} from '@arco-design/web-vue'
 import { IconMore } from '@arco-design/web-vue/es/icon'
-import { defineComponent } from 'vue'
+import { computed, defineComponent, h, compile } from 'vue'
 import { useI18n } from 'vue-i18n'
-
+import type { TableColumnData, TableData } from '@arco-design/web-vue/es/table/interface.d'
 export default defineComponent({
   setup() {
+    const userInfo = useUserStore()
     const { t } = useI18n()
+    const renderTag = (status: number) => {
+      if (status === -1) {
+        return `<a-tag  color="red" class='data-statistic-list-cover-tag'>
+              ${t('monitor.list.tag.auditFailed')}
+          </a-tag>`
+      }
+      return ''
+    }
+    const columns = computed(() => {
+      return [
+        {
+          title: t('monitor.list.title.order'),
+          render({ rowIndex }: { record: TableData; column: TableColumnData; rowIndex: number }) {
+            const tmp = `<span>${rowIndex + 1}</span>`
+            return h(compile(tmp))
+          }
+        },
+        {
+          title: t('monitor.list.title.cover'),
+          render({ record }: { record: TableData; column: TableColumnData; rowIndex: number }) {
+            const tmp = `<div class='data-statistic-list-cover-wrapper'>
+                <img src=${record.cover} />
+                ${renderTag(record.status)}
+              </div>`
+            return h(compile(tmp))
+          }
+        },
+        {
+          title: t('monitor.list.title.name'),
+          dataIndex: 'name'
+        },
+        {
+          dataIndex: 'duration',
+          title: t('monitor.list.title.duration')
+        },
+        {
+          dataIndex: 'id',
+          title: t('monitor.list.title.id')
+        }
+      ]
+    })
+    interface PreviewRecord {
+      cover: string
+      name: string
+      duration: string
+      id: string
+      status: number
+    }
+    const data: PreviewRecord[] = [
+      {
+        cover:
+          'http://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/c788fc704d32cf3b1136c7d45afc2669.png~tplv-uwbnlip3yd-webp.webp',
+        name: '视频直播',
+        duration: '00:05:19',
+        id: '54e23ade',
+        status: -1
+      }
+    ]
     return () => (
       <Space>
         <div>
-          <Card>
+          <Card class="general-card" title={t('monitor.title.studioPreview')}>
             {{
               default: () => (
                 <div>
-                  <img src="http://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/c788fc704d32cf3b1136c7d45afc2669.png~tplv-uwbnlip3yd-webp.webp" />
-                  <Space>
-                    <Avatar>
-                      <img src="" alt="" />
+                  <img
+                    style={{
+                      maxWidth: '600px'
+                    }}
+                    src="http://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/c788fc704d32cf3b1136c7d45afc2669.png~tplv-uwbnlip3yd-webp.webp"
+                  />
+                  <Space size="medium">
+                    <Avatar size={24}>
+                      <img src={userInfo.avatar} alt="alt" />
                     </Avatar>
-                    <Typography.Text></Typography.Text>
+                    <Typography.Text>
+                      {userInfo.name} {t('monitor.studioPreview.studio')}
+                    </Typography.Text>
                   </Space>
                   <Typography.Text>36,000 {t('monitor.studioPreview.watching')}</Typography.Text>
                 </div>
@@ -27,11 +105,20 @@ export default defineComponent({
             }}
           </Card>
         </div>
-        <div>
+        <Card class="general-card">
           <Tabs></Tabs>
-          <RadioGroup></RadioGroup>
-          <Table></Table>
-        </div>
+          <RadioGroup defaultValue="3" type="button">
+            <Radio value="1">{t('monitor.liveMethod.normal')}</Radio>
+            <Radio value="2">{t('monitor.liveMethod.flowControl')}</Radio>
+            <Radio value="3">{t('monitor.liveMethod.video')}</Radio>
+            <Radio value="4">{t('monitor.liveMethod.web')}</Radio>
+          </RadioGroup>
+          <Space>
+            <Link>{t('monitor.editCarousel')}</Link>
+            <Button disabled>{t('monitor.startCarousel')}</Button>
+          </Space>
+          <Table pagination={false} data={data} bordered={false}></Table>
+        </Card>
       </Space>
     )
   }
