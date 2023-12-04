0158818e82c2ab0030b9f93b82bd6054ef248b4a
manyuemeiquqi <940495614@qq.com>
2023-12-05 00:03:26 +0800

    feat: card list component

--- src/views/list/card-list/AddCard.tsx
+++ src/views/list/card-list/AddCard.tsx
@@ -1,7 +1,18 @@
+import { Card } from '@arco-design/web-vue'
+import { IconPlus } from '@arco-design/web-vue/es/icon'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   setup() {
-    return () => <div></div>
+    return () => (
+      <Card class="general-card" size="small">
+        <div>
+          <div>
+            <IconPlus />
+          </div>
+          <div>{props.description}</div>
+        </div>
+      </Card>
+    )
   }
 })
