8aa05850a97aad98ea6d74cee3bd2cda1a7c5fd7
manyuemeiquqi <940495614@qq.com>
2023-12-05 10:36:02 +0800

    feat: add char component prop style

--- src/components/chart-component/index.tsx
+++ src/components/chart-component/index.tsx
@@ -23,43 +23,7 @@ export default defineComponent({
   },
   setup(props) {
     const renderChart = ref<boolean>(false)
-    const option = ref({
-      title: {
-        text: 'Traffic Sources',
-        left: 'center'
-      },
-      tooltip: {
-        trigger: 'item',
-        formatter: '{a} <br/>{b} : {c} ({d}%)'
-      },
-      legend: {
-        orient: 'vertical',
-        left: 'left',
-        data: ['Direct', 'Email', 'Ad Networks', 'Video Ads', 'Search Engines']
-      },
-      series: [
-        {
-          name: 'Traffic Sources',
-          type: 'pie',
-          radius: '55%',
-          center: ['50%', '60%'],
-          data: [
-            { value: 335, name: 'Direct' },
-            { value: 310, name: 'Email' },
-            { value: 234, name: 'Ad Networks' },
-            { value: 135, name: 'Video Ads' },
-            { value: 1548, name: 'Search Engines' }
-          ],
-          emphasis: {
-            itemStyle: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: 'rgba(0, 0, 0, 0.5)'
-            }
-          }
-        }
-      ]
-    })
+
     nextTick(() => {
       renderChart.value = true
     })
@@ -67,11 +31,11 @@ export default defineComponent({
       renderChart.value && (
         <VChart
           style={{
-            width: '500px',
-            height: '500px'
+            width: props.width,
+            height: props.height
           }}
           option={props.options}
-          autoresize={true}
+          autoresize={props.autoResize}
         ></VChart>
       )
   }
