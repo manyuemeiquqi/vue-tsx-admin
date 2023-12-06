c05bc9ce3de077fa4bb6a33d3c7f6c07d8db31de
manyuemeiquqi <940495614@qq.com>
2023-12-06 10:22:05 +0800

    feat: fit i18n ally config

--- src/locale/en-US.ts
+++ src/locale/en-US.ts
@@ -1,13 +1,13 @@
-import dashboard from './en-US/dashboard'
-import detail from './en-US/detail'
-import exception from './en-US/exception'
-import form from './en-US/form'
-import list from './en-US/list'
-import login from './en-US/login'
-import result from './en-US/result'
-import settings from './en-US/settings'
-import user from './en-US/user'
-import visualization from './en-US/visualization'
+import dashboard from './en-US/dashboard.json'
+import detail from './en-US/detail.json'
+import exception from './en-US/exception.json'
+import form from './en-US/form.json'
+import list from './en-US/list.json'
+import login from './en-US/login.json'
+import result from './en-US/result.json'
+import settings from './en-US/settings.json'
+import user from './en-US/user.json'
+import visualization from './en-US/visualization.json'
 
 export default {
   'menu.dashboard': 'Dashboard',
--- src/locale/zh-CN.ts
+++ src/locale/zh-CN.ts
@@ -1,13 +1,13 @@
-import dashboard from './zh-CN/dashboard'
-import detail from './zh-CN/detail'
-import exception from './zh-CN/exception'
-import form from './zh-CN/form'
-import list from './zh-CN/list'
-import login from './zh-CN/login'
-import result from './zh-CN/result'
-import settings from './zh-CN/settings'
-import user from './zh-CN/user'
-import visualization from './zh-CN/visualization'
+import dashboard from './zh-CN/dashboard.json'
+import detail from './zh-CN/detail.json'
+import exception from './zh-CN/exception.json'
+import form from './zh-CN/form.json'
+import list from './zh-CN/list.json'
+import login from './zh-CN/login.json'
+import result from './zh-CN/result.json'
+import settings from './zh-CN/settings.json'
+import user from './zh-CN/user.json'
+import visualization from './zh-CN/visualization.json'
 
 export default {
   'menu.dashboard': '仪表盘',
