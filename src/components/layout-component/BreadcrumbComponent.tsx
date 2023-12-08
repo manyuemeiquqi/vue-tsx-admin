b12d0ce6703d9070156845c25378c106042750eb
manyuemeiquqi <940495614@qq.com>
2023-12-08 18:25:17 +0800

    feat: config breadcrumb

--- src/components/layout-component/BreadcrumbComponent.tsx
+++ src/components/layout-component/BreadcrumbComponent.tsx
@@ -1,12 +1,19 @@
 import { Breadcrumb } from '@arco-design/web-vue'
-import { defineComponent } from 'vue'
+import { defineComponent, ref, watch } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useRoute, type RouteLocationMatched } from 'vue-router'
 export default defineComponent({
   setup() {
-    const breadData: any[] = []
+    const route = useRoute()
+    const { t } = useI18n()
+    const breadData = ref<RouteLocationMatched[]>(route.matched)
+    watch(route, (val) => {
+      breadData.value = route.matched
+    })
     return () => (
       <Breadcrumb>
-        {breadData.map((item) => (
-          <Breadcrumb.Item></Breadcrumb.Item>
+        {breadData.value.map((item) => (
+          <Breadcrumb.Item>{t(item.meta.locale + '')}</Breadcrumb.Item>
         ))}
       </Breadcrumb>
     )
