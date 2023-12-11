676145a4bdc23a5d2facbb1d68d795288c5db416
manyuemeiquqi <940495614@qq.com>
2023-12-11 21:01:50 +0800

    feat: config vite base

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,11 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: '/Vtsc-admin/'
 })
