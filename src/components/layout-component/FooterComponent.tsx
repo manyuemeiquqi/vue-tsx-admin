5db340d6912ec31e2b41dad18ca457ad18c3fdd5
manyuemeiquqi <940495614@qq.com>
2023-12-16 16:25:03 +0800

    chore: gather applicationInfo

--- src/components/layout-component/FooterComponent.tsx
+++ src/components/layout-component/FooterComponent.tsx
@@ -1,10 +1,11 @@
+import { ApplicationInfo } from '@/types/enum'
 import { LayoutFooter } from '@arco-design/web-vue'
 import { defineComponent } from 'vue'
 export default defineComponent({
   setup() {
     return () => (
-      <LayoutFooter class="flex   h-10 justify-center items-center text-center text-[var(--color-text-2)]">
-        vue3 Admin 2023
+      <LayoutFooter class="flex h-10 justify-center items-center text-center text-[var(--color-text-2)]">
+        {ApplicationInfo.appTitle}
       </LayoutFooter>
     )
   }
