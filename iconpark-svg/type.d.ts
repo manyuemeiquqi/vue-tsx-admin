23b8e32c413248191bc4146bc7cbe59986880800
jinpeng <940495614@qq.com>
2023-12-20 14:51:44 +0800

    feat: add iconpark-svg

---
+++ iconpark-svg/type.d.ts
@@ -0,0 +1,13 @@
+
+interface IconParkIconElement extends HTMLElement {
+  'icon-id'?: '1079649';
+  name?: 'github-mark';
+  size?: string;
+  width?: string;
+  height?: string;
+  color?: string;
+  stroke?: string;
+  fill?: string;
+  rtl?: string;
+  spin?: string;
+}
