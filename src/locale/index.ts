ce6f62dfab02e33261c02f0eb0318e784fabb71f
jinpeng <940495614@qq.com>
2023-12-26 16:07:44 +0800

    feat: auto import i18n message

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,9 +1,22 @@
 import { createI18n } from 'vue-i18n'
-import en from './en-US'
-import cn from './zh-CN'
+
 import { LocalStorageKey, LocaleOptions } from '@/types/constants'
+import { get } from 'lodash'
 
 const defaultLocale = localStorage.getItem(LocalStorageKey.localeKey) || LocaleOptions.en
+
+const getMessageFromModules = (_moduleMap: Record<string, unknown>) => {
+  const ret = {}
+  for (const key in _moduleMap) {
+    let exportContent = get(_moduleMap[key], 'default')
+    if (exportContent) {
+      Object.assign(ret, exportContent)
+    }
+  }
+  return ret
+}
+const cnMessages = getMessageFromModules(import.meta.glob('./zh-CN/*.json', { eager: true }))
+const enMessages = getMessageFromModules(import.meta.glob('./en-US/*.json', { eager: true }))
 
 const i18n = createI18n({
   locale: defaultLocale,
@@ -11,8 +24,8 @@ const i18n = createI18n({
   legacy: false,
   allowComposition: true,
   messages: {
-    [LocaleOptions.en]: en,
-    [LocaleOptions.cn]: cn
+    [LocaleOptions.en]: enMessages,
+    [LocaleOptions.cn]: cnMessages
   }
 })
 
